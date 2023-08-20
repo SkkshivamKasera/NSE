385b4cef4f5010f28b86a04650fb9b46c8fc5a54
SHIVAM KASERA <skshivamkasera2003@gmail.com>
2023-08-20 16:25:30 +0530

    U

--- bot.js
+++ bot.js
@@ -10,7 +10,64 @@ const __dirname = dirname(__filename);
 
 
 const nseIndia = new NseIndia();
-const token = "6330218077:AAHdotEqxrfnMiqXIPvcVj64-ITzIYmhR2M";
+const token = process.env.BOT_TOKEN;
+
+function calculateStopLoss(entryPrice, currentPrice, riskPercentage) {
+    const priceDifference = entryPrice - currentPrice;
+    const stopLossAmount = (riskPercentage / 100) * entryPrice;
+    if (entryPrice < currentPrice) {
+        const stopLossLevel = entryPrice + priceDifference + stopLossAmount;
+        return stopLossLevel;
+    }
+    else {
+        const stopLossLevel = entryPrice - priceDifference - stopLossAmount;
+        return stopLossLevel;
+    }
+}
+
+function calculateStopLossDev(b, c, e) {
+    const d = b * e;
+    const stopLossLevel = c - d;
+    return stopLossLevel
+}
+
+function calculateStopLossWithCapital(Entry_Price, Risk_Percentage, Total_Capital) {
+    const Amount_at_Risk = (Risk_Percentage / 100) * Total_Capital
+    const stopLossLevel = Entry_Price - Amount_at_Risk
+    return stopLossLevel
+}
+
+function calculateStopLossPercentage(entryPrice, riskPercentage) {
+    if(riskPercentage <= 100){
+    const stopLossAmount = (riskPercentage / 100) * entryPrice;
+    const stopLossLevel = entryPrice - stopLossAmount;
+    return stopLossLevel;
+    }else{
+        return "Practicali Not Possible"
+    }
+}
+
+function calculateStopLossSupportResistance(entryPrice, supportLevel) {
+    const stopLossLevel = supportLevel - (entryPrice - supportLevel);
+    return stopLossLevel;
+}
+
+function calculateStopLossMovingAverage(entryPrice, movingAverage) {
+    const stopLossLevel = movingAverage - (entryPrice - movingAverage);
+    return stopLossLevel;
+}
+
+function calculateStopLossPattern(entryPrice, patternLow) {
+    const stopLossLevel = patternLow - (entryPrice - patternLow);
+    return stopLossLevel;
+}
+
+function calculateTrailingStopLoss(entryPrice, currentPrice, trailingPercentage) {
+    const priceDifference = entryPrice - currentPrice;
+    const trailingAmount = (trailingPercentage / 100) * entryPrice;
+    const stopLossLevel = entryPrice + priceDifference + trailingAmount;
+    return stopLossLevel;
+}
 
 let bot;
 const app = express()
@@ -23,17 +80,59 @@ app.get('/start', (req, res) => {
         bot.sendMessage(chatId, "Bot has started polling for updates.");
     });
 
-    bot.on("polling_error", (error) => {
-        console.error("Polling error:", error);
-    });
-
-    bot.on("message", (msg) => {
-        console.log("Received message:", msg);
-    });
-
-    bot.onText(/\/message/, (msg) => {
+    bot.onText(/\/sl/, (msg) => {
         const chatId = msg.chat.id
-        bot.sendMessage(chatId, "hii")
+        const values = msg.text.split(' ').slice(1);
+        const value1 = parseFloat(values[0]);
+        const value2 = parseFloat(values[1]);
+        const value3 = parseFloat(values[2]);
+        if(!isNaN(value1) && !isNaN(value2) && !isNaN(value3)){
+            const result = calculateStopLoss(value1, value2, value3)
+            bot.sendMessage(chatId, `${result}`)
+        }else{
+            bot.sendMessage(chatId, "Please Enter Valid Value")
+        }
+    })
+
+    bot.onText(/\/df/, (msg) => {
+        const chatId = msg.chat.id
+        const values = msg.text.split(' ').slice(1);
+        const value1 = parseFloat(values[0]);
+        const value2 = parseFloat(values[1]);
+        if(!isNaN(value1) && !isNaN(value2)){
+            const result = calculateStopLossPercentage(value1, value2)
+            bot.sendMessage(chatId, `${result}`)
+        }else{
+            bot.sendMessage(chatId, "Please Enter Valid Value")
+        }
+    })
+
+    bot.onText(/\/cp/, (msg) => {
+        const chatId = msg.chat.id
+        const values = msg.text.split(' ').slice(1);
+        const value1 = parseFloat(values[0]);
+        const value2 = parseFloat(values[1]);
+        const value3 = parseFloat(values[2]);
+        if(!isNaN(value1) && !isNaN(value2) && !isNaN(value3)){
+            const result = calculateStopLossWithCapital(value1, value2, value3)
+            bot.sendMessage(chatId, `${result}`)
+        }else{
+            bot.sendMessage(chatId, "Please Enter Valid Value")
+        }
+    })
+
+    bot.onText(/\/dev/, (msg) => {
+        const chatId = msg.chat.id
+        const values = msg.text.split(' ').slice(1);
+        const value1 = parseFloat(values[0]);
+        const value2 = parseFloat(values[1]);
+        const value3 = parseFloat(values[2]);
+        if(!isNaN(value1) && !isNaN(value2) && !isNaN(value3)){
+            const result = calculateStopLossDev(value1, value2, value3)
+            bot.sendMessage(chatId, `${result}`)
+        }else{
+            bot.sendMessage(chatId, "Please Enter Valid Value")
+        }
     })
 
     bot.startPolling()
