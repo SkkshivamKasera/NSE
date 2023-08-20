59a34e6267353a4f107243b7bfb3acd0ad33b18a
SHIVAM KASERA <skshivamkasera2003@gmail.com>
2023-08-20 12:27:36 +0530

    Please Ho Ja

--- bot.js
+++ bot.js
@@ -1,12 +1,23 @@
+import fs from 'fs';
 import { NseIndia } from 'stock-nse-india';
 import TelegramBot from 'node-telegram-bot-api';
 
 const nseIndia = new NseIndia();
 const token = "6330218077:AAHdotEqxrfnMiqXIPvcVj64-ITzIYmhR2M"; // Replace with your actual bot token
 
+const botInstanceFlagPath = './bot-instance-flag.json';
 let bot;
 
-if (!bot) {
+// Read the flag from the file
+let botInstanceFlag;
+try {
+    const fileContent = fs.readFileSync(botInstanceFlagPath, 'utf8');
+    botInstanceFlag = JSON.parse(fileContent);
+} catch (error) {
+    botInstanceFlag = { botCreated: false };
+}
+
+if (!botInstanceFlag.botCreated) {
     bot = new TelegramBot(token);
 
     bot.onText(/\/start/, (msg) => {
@@ -26,6 +37,10 @@ if (!bot) {
     bot.startPolling();
 
     console.log("Bot polling started.");
+
+    // Update the flag in the file
+    botInstanceFlag.botCreated = true;
+    fs.writeFileSync(botInstanceFlagPath, JSON.stringify(botInstanceFlag, null, 2), 'utf8');
 } else {
     console.log("Bot instance already exists.");
 }
